66fa713b27d87e5cedf4ad745620ba62b089f94b
Marquis <marquispierre27@gmail.com>
2022-05-08 20:42:13 -0400

    deleted some code

--- slideshow.js
+++ slideshow.js
@@ -15,5 +15,5 @@ function showSlides() {
     }
 
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); 
 }
